b675d81d26c4f1bdd14f1c2ae191c4f28c41b88f
PabloGM1204 <pablogm1204@gmail.com>
2024-05-15 09:35:49 +0200

    Creación de la pipe de la letra mayuscula

---
+++ src/app/shared/pipes/primera-letra-mayus.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PrimeraLetraMayusPipe } from './primera-letra-mayus.pipe';
+
+describe('PrimeraLetraMayusPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrimeraLetraMayusPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
